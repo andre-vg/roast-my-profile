639c9ae48d46bd1a228fbffc32125f9603d9167e
André Gonçalves <avictorg@gmail.com>
2025-09-29 21:30:23 -0300

    Initial Commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["roast-my-profile_*"],
+} satisfies Config;
